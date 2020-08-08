1898fd50de3c8a4d8f9b73943b7d7ba6f3d37602
blangwell <b.langwell@outlook.com>
2020-08-07 17:04:01 -0700

    [pokedex] First commit after ahonore codealong

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+// public or static, use public because of public dir
+app.use(express.static('public')) 
 
 // GET - main index of site
-app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+app.get('/', (req, res) => {
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151>';
   // Use request to call the API
   axios.get(pokemonUrl).then(response => {
+    console.log(response.data);
     let pokemon = response.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   });
