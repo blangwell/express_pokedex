9e8800e5072afc762292eb60954ad63e5100f9b1
blangwell <b.langwell@outlook.com>
2020-08-08 17:33:04 -0700

    [routes] Add pokemon.ejs and render via pokemon.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
+const pokemon = require('./models/pokemon');
 const app = express();
+const router = express.Router();  
 const port = process.env.PORT || 3000;
 
 app.use(require('morgan')('dev'));
@@ -14,7 +16,7 @@ app.use(express.static('public'))
 
 // GET - main index of site
 // pull pokemon from API via axios request axios.get()
-// await the response and render teh index.ejs file
+// await the response and render the index.ejs file
 // with a param of an object with the key pokemon and a value
 // that calls pokemon.slice() on the array returned from response.data.results
 app.get('/', (req, res) => {
@@ -30,6 +32,11 @@ app.get('/', (req, res) => {
 // Imports all routes from the pokemon routes file
 // this is middleware that allows us to render from the routes dir
 app.use('/pokemon', require('./routes/pokemon'));
+
+router.get('/pokemon', (req, res) => {
+  // let pokemon = 
+  res.render('pokemon', {pokemon: pokemon.name})
+})
 
 const server = app.listen(port, () => {
   console.log(`Server is running on ${port}`);
